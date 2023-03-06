79fcefa76751fc57d7cc6528bb8ef5d18970004b
Xiangshi Yin <xyin.teaching@gmail.com>
2023-03-06 04:43:29 +0000

    source config added

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,26 @@
 
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+source as (
+    select * from {{ source('stripe','payment') }}
+),
 
-from dbt-tutorial.stripe.payment 
+staged as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+
+    from source
+)
+
+select * from staged
+
+
+
 
